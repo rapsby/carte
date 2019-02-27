3496c1e59fd10be622943c585765b209dd2d947e
Crazy Snoopy <csnopy@gmail.com>
2019-02-27 19:29:41 +0900

    Bootstrap 및 Jquery 추가

--- src/main/java/com/o2o/action/server/rest/ChefController.java
+++ src/main/java/com/o2o/action/server/rest/ChefController.java
@@ -1,10 +1,7 @@
 package com.o2o.action.server.rest;
 
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
@@ -20,8 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.google.actions.api.App;
 import com.o2o.action.server.app.ChefApp;
-import com.o2o.action.server.db.MealDetail;
-import com.o2o.action.server.db.MealDetail.MealType;
 import com.o2o.action.server.db.MealMenu;
 import com.o2o.action.server.repo.DateMenuRepository;
 import com.o2o.action.server.repo.MealDetailRepository;
@@ -60,26 +55,20 @@ public class ChefController {
 	public @ResponseBody String simpleGet(HttpServletRequest request, HttpServletResponse response) {
 		System.out.println("Call!!");
 
-		// detail = detailRepository.save(detail);
-
-		MealMenu menu = new MealMenu();
-		menu.setServiceDate(Calendar.getInstance().getTime());
-		menu.setDessert("디저트");
-		menu.setMrice("잡곡밥");
-		menu.setSalad1("셀러드1");
-		menu.setSalad2("셀러드2");
-
-		List<MealDetail> details = new ArrayList<MealDetail>();
-		MealDetail detail = new MealDetail();
-		detail.setMealType(MealType.LAUNCH_1);
-		detail.setMenu1("메뉴1");
-		detail.setMenu2("메뉴2");
-		detail.setMenu3("메뉴3");
-		detail.setMenu4("메뉴4");
-		details.add(detail);
-
-		menu.setMeals(details);
-		repository.save(menu);
+		/*
+		 * MealMenu menu = new MealMenu();
+		 * menu.setServiceDate(Calendar.getInstance().getTime());
+		 * menu.setDessert("디저트"); menu.setMrice("잡곡밥"); menu.setSalad1("셀러드1");
+		 * menu.setSalad2("셀러드2");
+		 * 
+		 * menu = repository.save(menu);
+		 * 
+		 * MealDetail detail = new MealDetail(); detail.setMealMenu(menu);
+		 * detail.setMealType(MealType.LAUNCH_1); detail.setMenu1("메뉴1");
+		 * detail.setMenu2("메뉴2"); detail.setMenu3("메뉴3"); detail.setMenu4("메뉴4");
+		 * 
+		 * detail = detailRepository.save(detail);
+		 */
 
 		Iterable<MealMenu> menus = repository.findAll();
 		for (MealMenu dateMenu : menus) {
