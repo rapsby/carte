d2711844f45f26eb2e6dc6861daf583d8a7ca679
Crazy Snoopy <csnopy@gmail.com>
2019-02-27 11:22:50 +0900

    Initial Commit

---
+++ src/main/java/com/o2o/action/server/rest/SimpleActionController.java
@@ -0,0 +1,56 @@
+package com.o2o.action.server.rest;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.actions.api.App;
+import com.o2o.action.server.app.TestCapApp;
+
+@RestController
+public class SimpleActionController {
+	private final App testCap;
+
+	public SimpleActionController() {
+		testCap = new TestCapApp();
+	}
+
+	@RequestMapping(value = "/simple", method = RequestMethod.POST)
+	public @ResponseBody String simple(@RequestBody String body, HttpServletRequest request,
+			HttpServletResponse response) {
+		String jsonResponse = null;
+		try {
+			System.out.println("request : " + body);
+			jsonResponse = testCap.handleRequest(body, getHeadersMap(request)).get();
+			System.out.println("response : " + jsonResponse);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+
+		return jsonResponse;
+	}
+
+	private Map<String, String> getHeadersMap(HttpServletRequest request) {
+		Map<String, String> map = new HashMap<String, String>();
+
+		Enumeration<String> headerNames = request.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			String key = (String) headerNames.nextElement();
+			String value = request.getHeader(key);
+			map.put(key, value);
+		}
+		return map;
+	}
+}
