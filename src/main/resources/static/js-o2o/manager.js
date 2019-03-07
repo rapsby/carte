a789eceed9ede36465ae9fe98697a3a08817d2e1
Crazy Snoopy <csnopy@gmail.com>
2019-03-07 16:36:08 +0900

    메뉴 추가 기능 수정중..

--- src/main/resources/static/js-o2o/manager.js
+++ src/main/resources/static/js-o2o/manager.js
@@ -25,11 +25,52 @@ function reload() {
 
 			console.log(data);
             if (data != null && data != '') {
-                $('#meal-edit').show();
-                $('#meal-create').hide();
+                $('#meal-create').html("수정");
+                $('#dialog-add-title').html("메뉴 수정");
+
+                var launch1 = null;
+                var launch2 = null;
+                var dinner = null;
+
+                for (var k = 0; k < data.meals.length; k++) {
+                    if (data.meals[k].mealType == 'LAUNCH_1') {
+                        launch1 = data.meals[k];
+                    } else if (data.meals[k].mealType == 'LAUNCH_2') {
+                        launch2 = data.meals[k];
+                    } else if (data.meals[k].mealType == 'DINNER') {
+                        dinner = data.meals[k];
+                    }
+                }
+
+                if (launch1 != null) {
+                    for (var i = 1; i <= 7; i++) {
+                        if (launch1['menu' + i] == null || launch1['menu' + i] == '') {
+                            $('#table-l1-' + i).html('-');
+                        } else {
+                            $('#table-l1-' + i).html(launch1['menu' + i]);
+                        }
+                    }
+                } else {
+                    for (var i = 1; i <= 7; i++) {
+                        $('#table-l1-' + i).html('-');
+                    }
+                }
+                if (launch2 != null) {
+                    for (var i = 1; i <= 7; i++) {
+                        if (launch2['menu' + i] == null || launch2['menu' + i] == '') {
+                            $('#table-l2-' + i).html('-');
+                        } else {
+                            $('#table-l2-' + i).html(launch2['menu' + i]);
+                        }
+                    }
+                } else {
+                    for (var i = 1; i <= 7; i++) {
+                        $('#table-l2-' + i).html('-');
+                    }
+                }
             } else {
-                $('#meal-edit').hide();
-                $('#meal-create').show();
+                $('#meal-create').html("추가");
+                $('#dialog-add-title').html("메뉴 추가");
             }
 
 			//$('#meal-menu').html(html);
